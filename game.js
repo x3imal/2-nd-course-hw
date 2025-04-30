2c9063c0cab23f77443fef10b6a882b35a847ae5
alexeyasurov <aasurov@gmail.com>
2025-04-30 13:04:18 +0300

    hw 7: написал 2 игры

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+
+//Игра 1
 function randomNumberGame() {
     const secretNumb = Math.floor(Math.random() * 100) + 1;
     let userNum = 0;
@@ -15,6 +17,7 @@ function randomNumberGame() {
     }
 }
 
+//Игра 2
 function simpleArithmeticGame() {
     const operators = ['+', '-', '*', '/'];
 
@@ -52,4 +55,49 @@ function simpleArithmeticGame() {
         alert("Неправильно!");
     }
 }
+
+//Игра 3
+function turnTheTextOver () {
+    let userText = prompt("Введите текст!")
+    const reversed = userText.split('').reverse().join('');
+    alert(reversed);
+}
+
+
+//Игра4
+const quiz = [
+    {
+        question: "Какой цвет небо?",
+        options: ["1. Красный", "2. Синий", "3. Зеленый"],
+        correctAnswer: 2
+    },
+    {
+        question: "Сколько дней в неделе?",
+        options: ["1. Шесть", "2. Семь", "3. Восемь"],
+        correctAnswer: 2
+    },
+    {
+        question: "Сколько у человека пальцев на одной руке?",
+        options: ["1. Четыре", "2. Пять", "3. Шесть"],
+        correctAnswer: 2
+    }
+];
+
+function runQuiz() {
+    let score = 0;
+
+    for (let i = 0; i < quiz.length; i++) {
+        const q = quiz[i];
+        const userAnswer = prompt(`${q.question}\n${q.options.join('\n')}`);
+
+        if (parseInt(userAnswer) === q.correctAnswer) {
+            score++;
+        }
+    }
+
+    alert(`Вы правильно ответили на ${score} из ${quiz.length} вопросов!`);
+}
+
+
+
 
